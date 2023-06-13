03ce70716cab97e4a9e7923047cc1eb7bb6df55a
ITPLUS Academy <thanghoang064@gmail.com>
2023-06-13 21:45:06 +0700

    update project

--- controllers/AddProductController.js
+++ controllers/AddProductController.js
@@ -1,4 +1,20 @@
-window.AddProductController = function ($scope) {
-	
-	
+window.AddProductController = function ($scope,$http,$location) {
+    var apiUrl = 'http://localhost:3000/products';
+    $scope.onSubmitForm = function() {
+        // tạo ra đối tượng item mới để thêm vào 
+        var newItem =  {
+            ...$scope.inputValue,
+        }
+        //khi thêm dữ liệu mới thì phải sử dụng phương thức post
+        $http.post(
+            apiUrl,
+            newItem // dữ liệu để thêm vào 
+        ).then(function(response){
+            // trạng thái của tk thêm mới là 201
+            if(response.status == 201) {
+                $location.path('list-product')
+            }
+        })
+
+    }
 }
--- controllers/ListProductController.js
+++ controllers/ListProductController.js
@@ -1,4 +1,33 @@
-window.ListProductController = function ($scope) {
-	// $scope.title = "Đây là trang chủ";
+window.ListProductController = function ($scope,$http) {
+	
+	//tạo đường dẫn apiUrl để call tới API 
+	var apiUrl = "http://localhost:3000/products";
+	$scope.getData = function(){
+		//gọi để lấy data
+		$http.get(apiUrl).then(function(response){
+			// console.log(response);
+			//nếu như responnse trả về thành công 
+			if(response.status == 200) {
+				// tạo ra 1 biến products để hứng dữ liệu từ
+				// phía json-server trả về
+				$scope.products = response.data;
+			}
+		})
+	}
+	$scope.getData();
+	$scope.onDelete = function(deleteID) {
+		let confirm = window.confirm("Bạn có muốn xóa không ?");
+		// nếu như confirm ok tức là đồng ý xóa
+		if(confirm) {
+			$http.delete(`${apiUrl}/${deleteID}`).then(
+				function(response) {
+					if(response.status == 200) {
+						//gọi lại hàm getData để cập nhập lại bảng
+						$scope.getData();
+					}
+				}
+			)
+		}
+	}
 	
 }
